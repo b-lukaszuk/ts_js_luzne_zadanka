d4b832090431094ed98bce874353e30f5819b4b9
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-24 21:39:47 +0100

    kosmetyka

--- part1_Jan_2022/task1/arr1dPreFilled.ts
+++ part1_Jan_2022/task1/arr1dPreFilled.ts
@@ -1,4 +1,4 @@
-function getPreFilledArr1d(len: number, fill: number) {
+function getPreFilledArr1d(len: number, fill: number): Array<number> {
     let result = new Array(len);
     for (let i = 0; i < len; i++) {
         result[i] = fill;

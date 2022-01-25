d484b54706bbeab5e15b0ec50e482cd9cd1149e3
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-25 21:00:29 +0100

    kosmetyka

--- part1_Jan_2022/task2/scrambleArr1d.ts
+++ part1_Jan_2022/task2/scrambleArr1d.ts
@@ -1,8 +1,8 @@
-function scramble(arr1d: any[], times: number = 0): any[] {
+function scramble(arr1d: Array<number>, times: number = 0): Array<number> {
     // the Fisher-Yates algorithm
     // https://dev.to/codebubb/how-to-shuffle-an-array-in-javascript-2ikj
     if (times > (arr1d.length - 1) || times <= 0) { times = arr1d.length - 1 }
-    let arrCopy: any[] = [...arr1d];
+    let arrCopy: Array<number> = [...arr1d];
     for (let i = times; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         const temp = arrCopy[i];

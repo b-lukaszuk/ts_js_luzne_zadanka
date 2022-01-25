d8962ac1cf2255e9c0f898a5ba12fd5dd68a065a
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-25 20:45:53 +0100

    dodano plik main.ts + importy i podstawowy szkielet

---
+++ part1_Jan_2022/task2/main.ts
@@ -0,0 +1,19 @@
+import {
+    getArr1dPrefilledByFn,
+    getArr1dPrefilledWithNum
+} from './arr1dPreFilled';
+import randInt from './randInt';
+
+const noOfPrisoners: number = 100;
+const noOfCards: number = 100;
+const guessesPerPrisoner: number = 50;
+const noOfIterations: number = 10000;
+
+
+function main(): void {
+    console.log("====");
+    console.log("====");
+    console.log("That's all. Goodbye.");
+}
+
+main();

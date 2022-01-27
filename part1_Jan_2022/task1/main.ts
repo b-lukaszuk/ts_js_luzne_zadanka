bb5e1b178d4e017ac219c6736d0df1538fecc050
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-27 10:07:29 +0100

    kosmetyka, Array<number> -> number[]

--- part1_Jan_2022/task1/main.ts
+++ part1_Jan_2022/task1/main.ts
@@ -8,23 +8,23 @@ const daysInYear: number = 365;
 const noOfPeopleAtParty: number = 30;
 const noOfIters: number = 10000;
 
-function getPeopleAtParty(howMany: number): Array<number> {
-    let people: Array<number> = getArr1dPrefilledByFn(howMany, () => {
+function getPeopleAtParty(howMany: number): number[] {
+    let people: number[] = getArr1dPrefilledByFn(howMany, () => {
         return randInt(0, daysInYear);
     });
     return people;
 }
 
-function getBDaysCounts(bDays: Array<number>): Array<number> {
+function getBDaysCounts(bDays: number[]): number[] {
     let lenBDays: number = bDays.length;
-    let counts: Array<number> = getArr1dPrefilledWithNum(daysInYear, 0);
+    let counts: number[] = getArr1dPrefilledWithNum(daysInYear, 0);
     for (let i = 0; i < lenBDays; i++) {
         counts[bDays[i]] += 1;
     }
     return counts;
 }
 
-function anyValGT(arr1d: Array<number>, GTwhat: number): boolean {
+function anyValGT(arr1d: number[], GTwhat: number): boolean {
     for (let i = 0; i < arr1d.length; i++) {
         if (arr1d[i] > GTwhat) {
             return true;
@@ -33,20 +33,20 @@ function anyValGT(arr1d: Array<number>, GTwhat: number): boolean {
     return false;
 }
 
-function anySameBDays(bDays: Array<number>): boolean {
-    let counts: Array<number> = getBDaysCounts(bDays);
+function anySameBDays(bDays: number[]): boolean {
+    let counts: number[] = getBDaysCounts(bDays);
     return anyValGT(counts, 1);
 }
 
-function getAvg(arr1d: Array<number>): number {
+function getAvg(arr1d: number[]): number {
     let sum: number = arr1d.reduce((acc, curVal) => {
         return acc + curVal;
     })
     return sum / arr1d.length;
 }
 
-function runNIters(nIter: number): Array<number> {
-    let result: Array<number> = getArr1dPrefilledByFn(nIter, () => {
+function runNIters(nIter: number): number[] {
+    let result: number[] = getArr1dPrefilledByFn(nIter, () => {
         return anySameBDays(getPeopleAtParty(noOfPeopleAtParty)) ? 1 : 0;
     });
     return result;

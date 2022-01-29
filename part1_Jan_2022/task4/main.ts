36954436c62fb114a28231a8c9c9c0ce60618337
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-29 22:37:10 +0100

    dodano fnsy do szukania amicable pairs az do numeru

--- part1_Jan_2022/task4/main.ts
+++ part1_Jan_2022/task4/main.ts
@@ -43,3 +43,59 @@ function areAmicablePair(
         return acc && curVal;
     }, true);
 }
+
+function areArrsEql(arr1: number[], arr2: number[]): boolean {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function isSubArr(arr1d: number[], arr2d: number[][]): boolean {
+    for (let i = 0; i < arr2d.length; i++) {
+        if (areArrsEql(arr1d, arr2d[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function searchForAmicablePairs(upToExcl: number): void {
+    let results: number[][] = [];
+    for (let i = 1; i < upToExcl; i++) {
+        for (let j = 1; j < upToExcl; j++) {
+            if (
+                areAmicablePair(i, j, conditionsFns)
+                &&
+                !isSubArr([i, j].sort(), results)
+            ) {
+                results.push([i, j].sort());
+                console.log("Pair found:", [i, j].sort());
+            }
+        }
+    }
+}
+
+function displInfoAmicablePair(searchUpToExcl: number): void {
+    console.log("Looking for amicable pairs");
+    console.log("testing numbers up to (excl):", searchUpToExcl);
+    console.log("PLEASE BE PATIENT THIS MAY TAKE SOME TIME");
+    searchForAmicablePairs(searchUpToExcl);
+}
+
+function main(): void {
+    console.log("");
+    console.log("====");
+    displInfoAmicablePair(2000);
+    console.log("====");
+    console.log("That is all. Goodbye.");
+}
+
+// for nums up to 2000 it takes around 74 sec
+main();

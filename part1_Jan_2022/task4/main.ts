9b5f57c375e92dd93e9349816871494beff7f04e
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-01-31 17:09:49 +0100

    uproszcz., areAmicablePair, usun. helperow, update inf. o perform.

--- part1_Jan_2022/task4/main.ts
+++ part1_Jan_2022/task4/main.ts
@@ -6,40 +6,12 @@ function getSumPropDivs(testedNum: number): number {
     return getSum(propDivs);
 }
 
-const conditionsFns: Function[] = [
-    (num1, num2) => {
-        return num1 !== num2;
-    },
-    (num1, num2) => {
-        return getSumPropDivs(num1) === num2;
-    },
-    (num1, num2) => {
-        return getSumPropDivs(num2) === num1;
-    },
-];
-Object.freeze(conditionsFns);
-
-function areCondsMet(
-    num1: number,
-    num2: number,
-    condFns: Function[]
-): boolean[] {
-    let results: boolean[] = [];
-    for (let i = 0; i < condFns.length; i++) {
-        results.push(condFns[i](num1, num2));
-    }
-    return results;
-}
-
-function areAmicablePair(
-    num1: number,
-    num2: number,
-    predicates: Function[]
-): boolean {
-    let conditionsFilled: boolean[] = areCondsMet(num1, num2, predicates);
-    return conditionsFilled.reduce((acc, curVal) => {
-        return acc && curVal;
-    }, true);
+function areAmicablePair(num1: number, num2: number): boolean {
+    return (
+        num1 !== num2 &&
+        getSumPropDivs(num1) === num2 &&
+        getSumPropDivs(num2) === num1
+    );
 }
 
 function areArrsEql(arr1: number[], arr2: number[]): boolean {
@@ -68,11 +40,7 @@ function searchForAmicablePairs(upToExcl: number): void {
     let results: number[][] = [];
     for (let i = 1; i < upToExcl; i++) {
         for (let j = 1; j < upToExcl; j++) {
-            if (
-                areAmicablePair(i, j, conditionsFns)
-                &&
-                !isSubArr([i, j].sort(), results)
-            ) {
+            if (areAmicablePair(i, j) && !isSubArr([i, j].sort(), results)) {
                 results.push([i, j].sort());
                 console.log("Pair found:", [i, j].sort());
             }
@@ -95,5 +63,5 @@ function main(): void {
     console.log("That is all. Goodbye.");
 }
 
-// for nums up to 2000 it takes around 74 sec
+// for nums up to 2000 it takes around 40 sec
 main();

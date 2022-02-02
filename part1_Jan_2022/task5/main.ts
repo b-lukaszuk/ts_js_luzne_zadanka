0839edc41d95dba422fb701389118b5c9a8a7094
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-02 19:09:57 +0100

    dod. fn-sy getAnagramsOfWord i remAnagramsOfWord

--- part1_Jan_2022/task5/main.ts
+++ part1_Jan_2022/task5/main.ts
@@ -13,3 +13,15 @@ function isAnagram(word1: string, word2: string): boolean {
     let w2processed: string = toLowerAndSort(word2);
     return w1processed.localeCompare(w2processed) === 0;
 }
+
+function getAnagramsOfWord(word: string, dict: string[]): string[] {
+    return dict.filter((item) => {
+        return isAnagram(word, item);
+    });
+}
+
+function remAnagramsOfWord(word: string, dict: string[]): string[] {
+    return dict.filter((item) => {
+        return !isAnagram(word, item);
+    });
+}

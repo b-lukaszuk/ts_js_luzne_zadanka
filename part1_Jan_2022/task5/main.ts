c7ce86bcea6d03225ffc74857c9f28854054aef6
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-02 19:48:37 +0100

    dod. wczyt. listy slow z dysku (getWords) + wyp. znalez. anagramow

--- part1_Jan_2022/task5/main.ts
+++ part1_Jan_2022/task5/main.ts
@@ -40,3 +40,28 @@ function getMostAnagrams(dict: string[]): string[] {
     } while (dictCopy.length !== 0);
     return oldAnagrams;
 }
+
+function getWords(fileName: string): string[] {
+    try {
+        let fileContent: string = fs.readFileSync(fileName,
+            { encoding: "utf-8" });
+        return fileContent.split("\n");
+    } catch (error) {
+        console.log(
+            `Could not read data from file ${fileName}\nExiting the program.`
+        );
+        process.exit(1);
+    }
+}
+
+function main(): void {
+    console.log(`Reading list of words from ${fileName}`);
+    let dict: string[] = getWords(fileName);
+    console.log("looking for the greatest number of anagrams in the list");
+    console.log("PLEASE BE PATIENT THIS MAY TAKE SOME TIME");
+    console.log("Result:")
+    console.log(getMostAnagrams(dict));
+    console.log("That's all. Goodbye.");
+}
+
+main();

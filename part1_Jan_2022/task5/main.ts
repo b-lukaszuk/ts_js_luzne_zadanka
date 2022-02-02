4d95ca82ff9e626eced739b7a91bcf7225b494f2
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-02 19:08:27 +0100

    dodano fn. isAnagram + helper (preprocessing) toLowerAndSort

---
+++ part1_Jan_2022/task5/main.ts
@@ -0,0 +1,15 @@
+const fileName: string = "./unixdict.txt"
+
+function toLowerAndSort(word: string): string {
+    let lowerCaseLetters: string[] = word.toLocaleLowerCase().split('');
+    let sorted: string[] = lowerCaseLetters.sort((a, b) => {
+        return a.localeCompare(b);
+    })
+    return sorted.join('');
+}
+
+function isAnagram(word1: string, word2: string): boolean {
+    let w1processed: string = toLowerAndSort(word1);
+    let w2processed: string = toLowerAndSort(word2);
+    return w1processed.localeCompare(w2processed) === 0;
+}

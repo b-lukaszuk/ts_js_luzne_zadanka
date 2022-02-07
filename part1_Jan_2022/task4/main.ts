358f2e21c9b29e4dbc5d4a39ac9d74c905e280cb
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-07 19:21:03 +0100

    popr. perf., usun. niepotrz przejscia (for j) i dodatkowe checki

--- part1_Jan_2022/task4/main.ts
+++ part1_Jan_2022/task4/main.ts
@@ -14,35 +14,11 @@ function areAmicablePair(num1: number, num2: number): boolean {
     );
 }
 
-function areArrsEql(arr1: number[], arr2: number[]): boolean {
-    if (arr1.length !== arr2.length) {
-        return false;
-    } else {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] !== arr2[i]) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-function isSubArr(arr1d: number[], arr2d: number[][]): boolean {
-    for (let i = 0; i < arr2d.length; i++) {
-        if (areArrsEql(arr1d, arr2d[i])) {
-            return true;
-        }
-    }
-    return false;
-}
-
 function searchForAmicablePairs(upToExcl: number): void {
-    let results: number[][] = [];
     for (let i = 1; i < upToExcl; i++) {
-        for (let j = 1; j < upToExcl; j++) {
-            if (areAmicablePair(i, j) && !isSubArr([i, j].sort(), results)) {
-                results.push([i, j].sort());
-                console.log("Pair found:", [i, j].sort());
+        for (let j = i; j < upToExcl; j++) {
+            if (areAmicablePair(i, j)) {
+                console.log("Pair found:", [i, j]);
             }
         }
     }

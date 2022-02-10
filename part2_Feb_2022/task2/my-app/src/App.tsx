7a6e3b449277674a3b708718a30885a526258443
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-10 21:02:46 +0100

    dod. komponent Canvas zawierajacy tag canvas

--- part2_Feb_2022/task2/my-app/src/App.tsx
+++ part2_Feb_2022/task2/my-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Canvas from './canvas/Canvas';
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            Pendulum
+            <Canvas />
         </div>
     );
 }
---
+++ part2_Feb_2022/task2/my-app/src/canvas/Canvas.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Canvas.css';
+
+const Canvas: React.FC = () => {
+    return (
+        <div>
+            <canvas className="canvas" />
+        </div>
+    )
+}
+
+export default Canvas;

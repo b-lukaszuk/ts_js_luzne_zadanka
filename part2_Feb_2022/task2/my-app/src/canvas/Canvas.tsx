752856c9694afc57f0490509eae748da0eb06860
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-12 19:08:52 +0100

    dod. rys. prostego ksztaltu (prostokat) z useEffect + context 2d

--- part2_Feb_2022/task2/my-app/src/canvas/Canvas.tsx
+++ part2_Feb_2022/task2/my-app/src/canvas/Canvas.tsx
@@ -1,8 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Canvas.css';
 
 const Canvas: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    let colBlue: boolean = true;
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        if (canvas === null) { return undefined; }
+        const context = canvas.getContext("2d");
+        if (context === null) { return undefined; }
+        context.rect(0, 0, 100, 100);
+        context.fillStyle = colBlue ? "blue" : "red";
+        context.fill();
+    }, [])
+
     return (
         <div>
             <canvas ref={canvasRef} className="canvas" />

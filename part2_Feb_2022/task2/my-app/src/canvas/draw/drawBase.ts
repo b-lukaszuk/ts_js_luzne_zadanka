a35ca9ee451ea814f9c4b2213cc554a84f1ff701
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-12 21:22:34 +0100

    kosmetyka

--- part2_Feb_2022/task2/my-app/src/canvas/draw/drawBase.ts
+++ part2_Feb_2022/task2/my-app/src/canvas/draw/drawBase.ts
@@ -1,11 +1,11 @@
 import drawCircle from './drawCircle';
 import drawHline from './drawHline'
 
-function drawBase(canv: HTMLCanvasElement, ctx: CanvasRenderingContext2D): void {
+function drawBase(canv: HTMLCanvasElement, ctx: CanvasRenderingContext2D,
+    height: number): void {
     const lineCol: string = "#303030";
-    drawHline(canv, ctx, canv.height / 4, lineCol);
-    drawCircle(canv, ctx, canv.width / 2, canv.height / 4, 10, "darkgray");
-
+    drawHline(canv, ctx, height, lineCol);
+    drawCircle(canv, ctx, canv.width / 2, height, 10, "darkgray");
 }
 
 export default drawBase;

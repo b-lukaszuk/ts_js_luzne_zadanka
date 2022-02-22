4e767f5e2912163a447750ac2fcc348bcf91febe
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-02-22 22:21:09 +0100

    dodano liczenie funkcji gamma (uzywa Lanczos approximation)

---
+++ part2_Feb_2022/task6/utils/tTest/gamma.ts
@@ -0,0 +1,51 @@
+function isInt(n: number): boolean {
+    if (isNaN(n)) {
+        return false;
+    } else {
+        return (n | 0) === n;
+    }
+}
+
+function factorial(n: number): number {
+    if (n < 0 || !isInt(n)) {
+        throw Error("factorial can be computed only for integers >= 0");
+    } else if (n < 2) {
+        return 1;
+    } else {
+        let result: number = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+// https://rosettacode.org/wiki/Gamma_function#JavaScript
+// https://en.wikipedia.org/wiki/Lanczos_approximation
+// https://en.wikipedia.org/wiki/Gamma_function
+function gamma(x: number): number {
+    if (isInt(x)) {
+        return factorial(x - 1);
+    } else {
+        let p: number[] = [0.99999999999980993, 676.5203681218851,
+            -1259.1392167224028, 771.32342877765313, -176.61502916214059,
+            12.507343278686905, -0.13857109526572012,
+            9.9843695780195716e-6, 1.5056327351493116e-7];
+
+        let g: number = 7;
+        if (x < 0.5) {
+            return Math.PI / (Math.sin(Math.PI * x) * gamma(1 - x));
+        }
+
+        x -= 1;
+        let a: number = p[0];
+        let t: number = x + g + 0.5;
+        for (let i = 1; i < p.length; i++) {
+            a += p[i] / (x + i);
+        }
+
+        return Math.sqrt(2 * Math.PI) * Math.pow(t, x + 0.5) * Math.exp(-t) * a;
+    }
+}
+
+export default gamma;

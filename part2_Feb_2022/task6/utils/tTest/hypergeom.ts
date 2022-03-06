6782ef9062434ae6568288aa7fb531c2ff0b1bdb
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-06 18:39:52 +0100

    kosmetyka

--- part2_Feb_2022/task6/utils/tTest/hypergeom.ts
+++ part2_Feb_2022/task6/utils/tTest/hypergeom.ts
@@ -1,6 +1,8 @@
 import choose from "./choose";
 
 // https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.hypergeom.html
+// TODO: check it with R's hypergeom functions
+// (either: dhyper, phyper, qhyper; probably: dhyper)
 function hypergeom(sk: number, cM: number, sn: number, cN: number): number {
     let numerator1: number = choose(sn, sk);
     let numerator2: number = choose(cM - sn, cN - sk);

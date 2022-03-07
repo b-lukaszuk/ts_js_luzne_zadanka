c77e24f8087d9a8e9b68e6cacbbdc5a8cebbaeaa
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-07 23:01:19 +0100

    poprawka na zgodnosc z R-owym choose

--- part2_Feb_2022/task6/utils/basic/choose.ts
+++ part2_Feb_2022/task6/utils/basic/choose.ts
@@ -1,9 +1,16 @@
-import factorial from "../basic/factorial";
+import factorial from "./factorial";
+import isInt from "./isInt";
 
+// tested (with a few random numbers) for fidelity with R's choose function
+// quite accurate (agrees with R's output) diff. in range of 10-11 decimals
 function choose(n: number, k: number): number {
     if (k > n) {
         return 0
     } else {
+        if (!isInt(k)) {
+            console.warn(`k must be integer, rounded to ${Math.round(k)}`);
+            k = Math.round(k);
+        }
         return factorial(n) / (factorial(n - k) * factorial(k));
     }
 }

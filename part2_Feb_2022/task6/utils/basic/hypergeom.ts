fbc269589eaa78a79652ab5c06ab816ba2620d11
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-14 14:08:46 +0100

    kosmetyka

--- part2_Feb_2022/task6/utils/basic/hypergeom.ts
+++ part2_Feb_2022/task6/utils/basic/hypergeom.ts
@@ -1,13 +1,55 @@
 import choose from "./choose";
 
-// https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.hypergeom.html
 // TODO: check it with R's hypergeom functions
 // (either: dhyper, phyper, qhyper; probably: dhyper)
-function hypergeom(sk: number, cM: number, sn: number, cN: number): number {
-    let numerator1: number = choose(sn, sk);
-    let numerator2: number = choose(cM - sn, cN - sk);
-    let denominator: number = choose(cM, cN);
+// https://www.youtube.com/watch?v=uzN7U88KSx8
+// tested with the numbers given in the video, seems to be working correctly
+
+function hypergeom(
+    numOfSuccInNumOfTrials: number,
+    totalNoOfEltsInPopulation: number,
+    numOfSuccInPopulation: number,
+    numOfTrialsOrSampleSize: number
+): number {
+    let numOfFailuresInPopulation: number =
+        totalNoOfEltsInPopulation - numOfSuccInPopulation;
+    let numOfFailuresInNumOfTrials: number =
+        numOfTrialsOrSampleSize - numOfSuccInNumOfTrials;
+    let numerator1: number = choose(
+        numOfSuccInPopulation,
+        numOfSuccInNumOfTrials
+    );
+    let numerator2: number = choose(
+        numOfFailuresInPopulation,
+        numOfFailuresInNumOfTrials
+    );
+    let denominator: number = choose(
+        totalNoOfEltsInPopulation,
+        numOfTrialsOrSampleSize
+    );
+    // console.log(
+    //     "numerator 1 =",
+    //     numOfSuccInPopulation,
+    //     numOfSuccInNumOfTrials
+    // );
+    // console.log(
+    //     "numerator 2 =",
+    //     numOfFailuresInPopulation,
+    //     numOfFailuresInNumOfTrials
+    // );
+    // console.log(
+    //     "denominator =",
+    //     totalNoOfEltsInPopulation,
+    //     numOfTrialsOrSampleSize
+    // );
     return (numerator1 * numerator2) / denominator;
 }
+
+// [0, 1, 2, 3].forEach((n) => console.log(hypergeom(n, 9, 3, 5)));
+// console.log(
+//     [0, 1, 2, 3]
+//         .map((n) => hypergeom(n, 9, 3, 5))
+//         .reduce((prev, cur) => prev + cur, 0)
+// );
 
 export default hypergeom;

8a9773fba1cd1897050c9b0959ee6a2c9b6acaa7
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-19 14:51:14 +0100

    kosmetyka

--- part3_Mar_2022/task1/degree.ts
+++ part3_Mar_2022/task1/degree.ts
@@ -5,20 +5,20 @@ function d2t(degrees: number): number {
 
 function d2g(degrees: number): number {
     let turns: number = d2t(degrees);
-    let gradian: number = turns * 400;
-    return gradian;
+    let gradians: number = turns * 400;
+    return gradians;
 }
 
 function d2m(degrees: number): number {
     let turns: number = d2t(degrees);
-    let mil: number = turns * 6400;
-    return mil;
+    let mils: number = turns * 6400;
+    return mils;
 }
 
 function d2r(degrees: number): number {
     let turns: number = d2t(degrees);
-    let radian: number = turns * 2 * Math.PI;
-    return radian;
+    let radians: number = turns * 2 * Math.PI;
+    return radians;
 }
 
 export { d2g, d2m, d2r };

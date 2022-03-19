ae3a6425635d4639d926dceb2f5d98489f25bf5d
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-19 15:06:08 +0100

    dodano funkcje konwertujace mile do innych skal

---
+++ part3_Mar_2022/task1/mil.ts
@@ -0,0 +1,24 @@
+function m2t(mils: number): number {
+    let turns: number = mils / 6400;
+    return turns;
+}
+
+function m2d(mils: number): number {
+    let turns: number = m2t(mils);
+    let degrees: number = turns * 360;
+    return degrees;
+}
+
+function m2g(mils: number): number {
+    let turns: number = m2t(mils);
+    let gradians: number = turns * 400;
+    return gradians;
+}
+
+function m2r(mils: number): number {
+    let turns: number = m2t(mils);
+    let radians: number = turns * 2 * Math.PI;
+    return radians;
+}
+
+export { m2d, m2g, m2r };

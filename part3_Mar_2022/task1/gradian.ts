f86055bac66130101830cecf0cc920e5743e6e33
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-19 15:00:10 +0100

    dodano funkcje konwertujace gradiany do innych skal

---
+++ part3_Mar_2022/task1/gradian.ts
@@ -0,0 +1,24 @@
+function g2t(gradians: number): number {
+    let turns: number = gradians / 400;
+    return turns;
+}
+
+function g2d(gradians: number): number {
+    let turns: number = g2t(gradians);
+    let degrees: number = turns * 360;
+    return degrees;
+}
+
+function g2m(gradians: number): number {
+    let turns: number = g2t(gradians);
+    let mils: number = turns * 6400;
+    return mils;
+}
+
+function g2r(gradians: number): number {
+    let turns: number = g2t(gradians);
+    let radians: number = turns * 2 * Math.PI;
+    return radians;
+}
+
+export { g2d, g2m, g2r };

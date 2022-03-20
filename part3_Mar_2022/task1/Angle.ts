7edfb924e288cc79a0c9d67982df82036605fc74
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-03-20 18:34:02 +0100

    dodano klase Angle, internale + two-way conversion do stopni

---
+++ part3_Mar_2022/task1/Angle.ts
@@ -0,0 +1,36 @@
+import { d2t } from "./degree";
+import { t2d } from "./turns";
+
+class Angle {
+    private _turns: number = 0;
+    private _negative: boolean = false;
+
+    constructor(turns: number = 0) {
+        this._setInternals(turns);
+    }
+
+    private _setInternals(turns: number = 0): void {
+        turns = Math.abs(turns) > 1 ? turns % 1 : turns;
+        if (turns < 0) {
+            this._turns = 1 - turns;
+            this._negative = true;
+        } else {
+            this._turns = turns;
+            this._negative = false;
+        }
+    }
+
+    private _getTurns(): number {
+        return this._negative ? (1 - this._turns) : this._turns;
+    }
+
+    public set degrees(degrees: number) {
+        this._setInternals(d2t(degrees));
+    }
+
+    public get degrees(): number {
+        return t2d(this._getTurns());
+    }
+}
+
+export default Angle;

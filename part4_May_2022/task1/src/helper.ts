4bb764158a30f807e1fbdc65f955cdcb54e72864
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-03 09:36:49 +0200

    kosmetyka, usunieto testowy plik (cors policy) helper.ts

--- part4_May_2022/task1/src/helper.ts
+++
@@ -1,7 +0,0 @@
-function helloHelper(): void {
-    console.log("hello there from helper file");
-    console.log("and again, to check if everything is ok");
-}
-
-export default helloHelper;
-
--- part4_May_2022/task1/src/index.ts
+++ part4_May_2022/task1/src/index.ts
@@ -1,5 +1,1 @@
-import helloHelper from "./helper.js";
-
 console.log("hello there from ts/js file");
-
-helloHelper();

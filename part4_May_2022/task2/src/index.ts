9b61ed21d9e7adf6514b6435fdae79085a93a391
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-21 14:04:19 +0200

    kosmetyka

--- part4_May_2022/task2/src/index.ts
+++ part4_May_2022/task2/src/index.ts
@@ -2,10 +2,10 @@ let elements: string[] = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", 
 
 let molecularMasses: number[] = [1.00797, 4.0026, 6.941, 9.01218, 10.81, 12.011, 14.0067, 15.9994, 18.998403, 20.179, 22.98977, 24.305, 26.98154, 28.0855, 30.97376, 32.06, 35.453, 39.0983, 39.948, 40.08, 44.9559, 47.9, 50.9415, 51.996, 54.938, 55.847, 58.7, 58.9332, 63.546, 65.38, 69.72, 72.59, 74.9216, 78.96, 79.904, 83.8, 85.4678, 87.62, 88.9059, 91.22, 92.9064, 95.94, 98, 101.07, 102.9055, 106.4, 107.868, 112.41, 114.82, 118.69, 121.75, 126.9045, 127.6, 131.3, 132.9054, 137.33, 138.9055, 140.12, 140.9077, 144.24, 145, 150.4, 151.96, 157.25, 158.9254, 162.5, 164.9304, 167.26, 168.9342, 173.04, 174.967, 178.49, 180.9479, 183.85, 186.207, 190.2, 192.22, 195.09, 196.9665, 200.59, 204.37, 207.2, 208.9804, 209, 210, 222, 223, 226.0254, 227.0278, 231.0359, 232.0381, 237.0482, 238.029, 242, 243, 247, 247, 250, 251, 252, 255, 256, 257, 258, 260, 261, 262, 262, 263, 269, 272, 277];
 
-let elementMass: Map<string, number> = new Map();
+let elementsMasses: Map<string, number> = new Map();
 
 for (let i = 0; i < elements.length; i++) {
-    elementMass.set(elements[i], molecularMasses[i]);
+    elementsMasses.set(elements[i], molecularMasses[i]);
 }
 
 function splitSimplMolecToAtomsNums(molecule: string): RegExpMatchArray | null {
@@ -26,7 +26,7 @@ function isNum(text: string): boolean {
 }
 
 function getMolecularMass(element: string, defaultMass: number = 0): number {
-    let result: number | undefined = elementMass.get(element)
+    let result: number | undefined = elementsMasses.get(element);
     return result ? result : defaultMass;
 }
 
@@ -56,10 +56,10 @@ function calculateMassOfSimpleMolecule(molecule: string): number {
     return mass;
 }
 
-let molecule1: string = "C6H12O6"
-let molecule2: string = "H3PO4"
-let molecule3: string = "C9H11NO3"
-let molecule4: string = "CaCl2"
+let molecule1: string = "C6H12O6";
+let molecule2: string = "H3PO4";
+let molecule3: string = "C9H11NO3";
+let molecule4: string = "CaCl2";
 
 console.log(molecule1, calculateMassOfSimpleMolecule(molecule1));
 console.log(molecule2, calculateMassOfSimpleMolecule(molecule2));

5250682aa58cf26cea8249f28d4f659b7e045980
Bartek Lukaszuk <bartek.lukaszuk@o2.pl>
2022-05-26 23:14:20 +0200

    dodano mape mas aminokwasow

---
+++ part4_May_2022/task3/build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("task3 init");
+let aminoAcids = ["A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"];
+let molecularMasses = [89.094, 174.204, 132.119, 133.103, 121.15, 147.130, 146.146, 75.067, 155.157, 131.175, 146.190, 149.21, 165.192, 115.132, 105.093, 119.120, 204.229, 181.191, 117.148];
+let aminoAcidMasses = new Map();
+for (let i = 0; i < aminoAcids.length; i++) {
+    aminoAcidMasses.set(aminoAcids[i], molecularMasses[i]);
+}

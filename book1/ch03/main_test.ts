a11d6268adc14a0eb1119f61de8d7cf0641fe605
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2024-10-18 20:37:46 +0200

    added a few simple tests for book1/ch03/

--- book1/ch03/main_test.ts
+++ book1/ch03/main_test.ts
@@ -1,6 +1,27 @@
 import { assertEquals } from "@std/assert";
-// import { add } from "./main.ts";
+import { countCharInText } from "./main.ts";
 
-// Deno.test(function addTest() {
-//   assertEquals(add(2, 3), 5);
-// });
+Deno.test("is 'B' in 'bbbbbbb'? case insensitive", () => {
+  let txt: string = "bbbbbbb";
+  assertEquals(countCharInText(txt, "B", false), 0);
+});
+
+Deno.test("is 'B' in 'bbbbbbb'? case sensitive", () => {
+  let txt: string = "bbbbbbb";
+  assertEquals(countCharInText(txt, "B", true), txt.length);
+});
+
+Deno.test("is 'b' in 'bbbbbbb'? case insensitive", () => {
+  let txt: string = "bbbbbbb";
+  assertEquals(countCharInText(txt, "b", false), txt.length);
+});
+
+Deno.test("is 'k' in 'bbbbbbb'? case insensitive", () => {
+  let txt: string = "bbbbbbb";
+  assertEquals(countCharInText(txt, "k", false), 0);
+});
+
+Deno.test("is 'k' in 'bbbbbbb'? case sensitive", () => {
+  let txt: string = "bbbbbbb";
+  assertEquals(countCharInText(txt, "k", true), 0);
+});

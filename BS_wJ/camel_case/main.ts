d419e6d1bfcd98334255639b4ea25dc2350e0c73
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-05-24 18:56:22 +0200

    added part of solution to BS_wJ/camel_case/

--- BS_wJ/camel_case/main.ts
+++ BS_wJ/camel_case/main.ts
@@ -1,8 +1,23 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  main();
+}
+
+function printIntro() {
+  console.log(
+    "Toy program for converting snake_case to lowerCamelCase (and vice versa).",
+  );
+  console.log("Warning. It may or may not work correctly.\n");
+}
+
+function printOutro() {
+  console.log("\nThat's all. Goodbye!");
+}
+
+function main() {
+  printIntro();
+
+  // TODO: print examples
+
+  printOutro();
 }

b8d0330fb6884f9ab40f722231cf5a20e5d4365c
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-05-24 19:31:22 +0200

    added part of solution to BS_wJ/camel_case/

--- BS_wJ/camel_case/main.ts
+++ BS_wJ/camel_case/main.ts
@@ -3,6 +3,42 @@ if (import.meta.main) {
   main();
 }
 
+function isUppercase(letter: string): boolean {
+  return letter == letter.toLocaleUpperCase();
+}
+
+function changeToSnakeCase(camelCasedWord: string): string {
+  let result: string = "";
+  let c: string = "";
+  for (let i = 0; i < camelCasedWord.length; i++) {
+    c = camelCasedWord[i];
+    result += isUppercase(c) ? "_" + c.toLocaleLowerCase() : c;
+  }
+  return result;
+}
+
+function changeToCamelCase(snakeCasedWord: string): string {
+  let result: string = "";
+  let prevUnderscore: boolean = false;
+  let c: string = "";
+  for (let i = 0; i < snakeCasedWord.length; i++) {
+    c = snakeCasedWord[i];
+    if (c == "_") {
+      prevUnderscore = true;
+      continue;
+    }
+    result += prevUnderscore ? c.toLocaleUpperCase() : c;
+    prevUnderscore = false;
+  }
+  return result;
+}
+
+function printExamples(v: string[], converter: (s: string) => string) {
+  for (let i = 0; i < v.length; i++) {
+    console.log(v[i], " => ", converter(v[i]));
+  }
+}
+
 function printIntro() {
   console.log(
     "Toy program for converting snake_case to lowerCamelCase (and vice versa).",
@@ -17,7 +53,19 @@ function printOutro() {
 function main() {
   printIntro();
 
-  // TODO: print examples
+  const camels: string[] = [
+    "helloWorld",
+    "niceToMeetYou",
+    "translateToEnglish",
+  ];
+  const snakes: string[] = [
+    "hello_world",
+    "nice_to_meet_you",
+    "translate_to_english",
+  ];
+  printExamples(camels, changeToSnakeCase);
+  console.log();
+  printExamples(snakes, changeToCamelCase);
 
   printOutro();
 }

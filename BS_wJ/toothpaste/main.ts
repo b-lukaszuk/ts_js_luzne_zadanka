3d273d662ca3555894ed49146bc358cf54d82d34
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-05-25 19:54:25 +0200

    added solution to BS_wJ/toothpaste/

--- BS_wJ/toothpaste/main.ts
+++ BS_wJ/toothpaste/main.ts
@@ -1,6 +1,36 @@
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  main();
+class Cylinder {
+  radius: number;
+  height: number;
+
+  constructor(r: number, h: number) {
+    this.radius = r;
+    this.height = h;
+  }
+
+  getVolume(): number {
+    return this.height * Math.PI * this.radius ** 2;
+  }
+
+  toString(): string {
+    return `Cyl(r: ${this.radius}, h: ${this.height})`;
+  }
+}
+
+function getRatios(
+  cylinders: Cylinder[],
+  radiusChange: number,
+  heightChange: number,
+): number[] {
+  const ratios: number[] = [];
+  cylinders.forEach((c) => {
+    const newRadius = c.radius + radiusChange;
+    const newHeight = c.height + heightChange;
+    const c2 = new Cylinder(newRadius, newHeight);
+    const v1: number = c.getVolume();
+    const v2: number = c2.getVolume();
+    ratios.push(v2 / v1);
+  });
+  return ratios;
 }
 
 function printIntro() {
@@ -10,8 +40,22 @@ function printIntro() {
   console.log("Warning. It may or may not work correctly.\n");
 }
 
-function printExamples() {
-  console.log("Examples go here.");
+function printExamples(radiusChange: number, heightChange: number) {
+  const cyls: Cylinder[] = [1, 2, 3, 4, 5].map((r) => new Cylinder(r, 5));
+  const ratios: string[] = getRatios(cyls, radiusChange, heightChange).map(
+    (r) => (r * 100).toFixed(0) + "%",
+  );
+  console.log("Initial cylinders:", cyls.toString());
+  console.log(
+    "Changing radius by",
+    radiusChange,
+    "and height by",
+    heightChange,
+  );
+  console.log(
+    "The obtained ratios for volumes of the cylinders (after / before) are:",
+    ratios.toString(),
+  );
 }
 
 function printOutro() {
@@ -21,7 +65,16 @@ function printOutro() {
 function main() {
   printIntro();
 
-  printExamples();
+  printExamples(1, 0);
+  console.log();
+  printExamples(1, -1);
+  console.log();
+  printExamples(1, -2);
 
   printOutro();
+}
+
+// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+  main();
 }

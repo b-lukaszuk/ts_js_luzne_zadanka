5cff8a0d4b7b0da8973fbc47b092d1c8aa188846
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-05-25 18:09:43 +0200

    added initial contents to BS_wJ/toothpaste/main.ts

--- BS_wJ/toothpaste/main.ts
+++ BS_wJ/toothpaste/main.ts
@@ -1,8 +1,27 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
-
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  main();
+}
+
+function printIntro() {
+  console.log(
+    "Toy program for the toothpaste problem.",
+  );
+  console.log("Warning. It may or may not work correctly.\n");
+}
+
+function printExamples() {
+  console.log("Examples go here.");
+}
+
+function printOutro() {
+  console.log("\nThat's all. Goodbye!");
+}
+
+function main() {
+  printIntro();
+
+  printExamples();
+
+  printOutro();
 }

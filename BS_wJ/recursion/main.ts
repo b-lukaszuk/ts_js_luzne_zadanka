f8042366efdbd232649820017642f76175bb3406
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-19 10:31:26 +0200

    added print{Intro|Outro|Solution} and main to BS_wJ/recursion/main.ts with init contents

--- BS_wJ/recursion/main.ts
+++ BS_wJ/recursion/main.ts
@@ -1,8 +1,31 @@
-export function add(a: number, b: number): number {
+function add(a: number, b: number): number {
   return a + b;
+}
+
+function printIntro() {
+  console.log("Toy program for the recursion problem.");
+  console.log("Warning. It may or may not work correctly.\n");
+}
+
+function printSolution() {
+  console.log("The solution goes here.");
+}
+
+function printOutro() {
+  console.log("\nThat's all. Goodbye!");
+}
+
+function main() {
+  printIntro();
+
+  printSolution();
+
+  printOutro();
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  main();
 }
+
+export { add };

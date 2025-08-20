a286050f570122f6a84b0fe11f1bcf491e4fa44c
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-20 15:28:15 +0200

    added fn recFib to BS_wJ/recursion/main.ts

--- BS_wJ/recursion/main.ts
+++ BS_wJ/recursion/main.ts
@@ -9,6 +9,16 @@ function recFactorial(n: number): number {
   return n == 1 ? 1 : n * recFactorial(n - 1);
 }
 
+function recFib(n: number, lookup: Map<number, number>): number {
+  if (n < 0 || n > 40) {
+    throw new Error("n must be in range [0-40]");
+  }
+  if (!lookup.has(n)) {
+    lookup.set(n, recFib(n - 2, lookup) + recFib(n - 1, lookup));
+  }
+  return lookup.get(n);
+}
+
 function printIntro() {
   console.log("Toy program for the recursion problem.");
   console.log("Warning. It may or may not work correctly.\n");
@@ -17,6 +27,12 @@ function printIntro() {
 function printSolution() {
   for (let i = 1; i < 10; i++) {
     console.log(`factorial(${i}) = ${recFactorial(i)}`);
+  }
+  let map = new Map();
+  map.set(0, 0);
+  map.set(1, 1);
+  for (let i = 0; i < 10; i++) {
+    console.log(`Fib(${i}) = ${recFib(i, map)}`);
   }
 }
 

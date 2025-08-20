bee4a01f856bcb6823f12b36098b5f72b9e79290
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-20 15:12:44 +0200

    added fn recFActorial with examples

--- BS_wJ/recursion/main.ts
+++ BS_wJ/recursion/main.ts
@@ -2,13 +2,22 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
+function recFactorial(n: number): number {
+  if (n < 1) {
+    throw new Error("n must be greater than 0");
+  }
+  return n == 1 ? 1 : n * recFactorial(n - 1);
+}
+
 function printIntro() {
   console.log("Toy program for the recursion problem.");
   console.log("Warning. It may or may not work correctly.\n");
 }
 
 function printSolution() {
-  console.log("The solution goes here.");
+  for (let i = 1; i < 10; i++) {
+    console.log(`factorial(${i}) = ${recFactorial(i)}`);
+  }
 }
 
 function printOutro() {

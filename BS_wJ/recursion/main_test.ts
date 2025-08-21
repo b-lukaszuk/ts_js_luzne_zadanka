f2feb51b24d9188abd5aa3c4d03514e99e6e9427
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-21 20:53:37 +0200

    added basicErrors to BS_wJ/recursion/main_test.ts

--- BS_wJ/recursion/main_test.ts
+++ BS_wJ/recursion/main_test.ts
@@ -1,6 +1,16 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertThrows } from "@std/assert";
+import { recFactorial, recFib } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test(function basicErrors() {
+  let map = new Map();
+  map.set(0, 0);
+  map.set(1, 1);
+
+  assertThrows(() => recFactorial(-1));
+  assertThrows(() => recFactorial(-0.01));
+  assertThrows(() => recFactorial(0));
+
+  assertThrows(() => recFib(-100, map));
+  assertThrows(() => recFib(-1, map));
+  assertThrows(() => recFib(-0.01, map));
 });

c5df086638722c935f390bb435a7b17d59b42844
Bartlomiej Lukaszuk <bartek.lukaszuk@o2.pl>
2025-08-21 21:09:22 +0200

    cosmetic changes in BS_wJ/recursion/main_test.ts

--- BS_wJ/recursion/main_test.ts
+++ BS_wJ/recursion/main_test.ts
@@ -1,31 +1,37 @@
 import { assertEquals, assertThrows } from "@std/assert";
 import { recFactorial, recFib } from "./main.ts";
 
-Deno.test(function basicErrors() {
-  let map = new Map();
-  map.set(0, 0);
-  map.set(1, 1);
-
+Deno.test(function errorsFacts() {
   assertThrows(() => recFactorial(-1));
   assertThrows(() => recFactorial(-0.01));
   assertThrows(() => recFactorial(0));
-
-  assertThrows(() => recFib(-100, map));
-  assertThrows(() => recFib(-1, map));
-  assertThrows(() => recFib(-0.01, map));
 });
 
-Deno.test(function basicEquals() {
+Deno.test(function errorsFibs() {
+  function errorsFibs() {
+    let map = new Map();
+    map.set(0, 0);
+    map.set(1, 1);
+
+    assertThrows(() => recFib(-100, map));
+    assertThrows(() => recFib(-1, map));
+    assertThrows(() => recFib(-0.01, map));
+  }
+});
+
+Deno.test(function equalsFacts() {
   let facts = [0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800];
+  for (let i = 1; i <= 10; i++) {
+    assertEquals(recFactorial(i), facts[i]);
+  }
+});
+
+Deno.test(function equalsFibs() {
   let fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
 
   let map = new Map();
   map.set(0, 0);
   map.set(1, 1);
-
-  for (let i = 1; i <= 10; i++) {
-    assertEquals(recFactorial(i), facts[i]);
-  }
 
   for (let i = 0; i <= 10; i++) {
     assertEquals(recFib(i, map), fibs[i]);
